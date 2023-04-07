f852e3b640166f25c92c38ea238ec3ae6ccbe652
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-04-07 16:07:15 -0500

    added framer motion animations

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import AboutMe from './img/aboutMe.jpg'
+import { motion } from 'framer-motion'
 
 const AboutInfo = () => {
   return (
     <div className='aboutInfo'>
         <div className="aboutInfoItems container">
-            <img src={AboutMe} alt="" />
-            <h1 className="ribbon">
+            <motion.img 
+            animate={{scale: 1}} 
+            initial={{scale: 0}} 
+            transition={{duration: 0.35}}
+            src={AboutMe} 
+            alt="" />
+            <motion.h1 
+            transition={{duration: 0.35}} 
+            initial={{width: '0%'}} 
+            animate={{width: '40%'}} 
+            className="ribbon">
                 <strong className="ribbon-content">Front end Developer</strong>
-            </h1>
-            <div className="aboutInfoParagraph">
+            </motion.h1>
+            <motion.div 
+            animate={{scale: 1}} 
+            initial={{scale: 0}} 
+            transition={{duration: 0.35}}
+            className="aboutInfoParagraph">
                 <p>Hi, my name is Darion Jones! I am a front end Developer from Stillwater, Oklahoma. I love to learn and build complex web applications!</p>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
